f20414a93aeba4276ffbff3634857e37abb4fd30
rocha <rochatj.contact@gmail.com>
2017-06-02 16:04:20 +0200

    First commit

---
+++ serveur/uploads/networks/twitter3.py
@@ -0,0 +1,24 @@
+def build_cnn(x, y, input_var=None):
+	import lasagne
+	
+	network = lasagne.layers.InputLayer(shape=(None, x[0], x[1]),
+										input_var=input_var)
+	
+	network = lasagne.layers.Conv1DLayer(
+			network, num_filters=128, filter_size=3,
+			nonlinearity=lasagne.nonlinearities.rectify,
+			W=lasagne.init.GlorotUniform())
+			
+	network = lasagne.layers.MaxPool1DLayer(network, pool_size=2)
+
+	network = lasagne.layers.DenseLayer(
+			lasagne.layers.dropout(network, p=.5),
+			num_units=32,
+			nonlinearity=lasagne.nonlinearities.rectify)
+
+	network = lasagne.layers.DenseLayer(
+			lasagne.layers.dropout(network, p=.5),
+			num_units=y,
+			nonlinearity=lasagne.nonlinearities.softmax)
+
+	return network
