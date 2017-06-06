cd408aa7e75734bd1619281770f98d3e1f2ea423
rocha <rochatj.contact@gmail.com>
2017-06-06 10:23:13 +0200

    Debug

---
+++ serveur/uploads/networks/twitter10.py
@@ -0,0 +1,22 @@
+def build_cnn(x, y, input_var=None):
+	import lasagne
+	
+	network = lasagne.layers.InputLayer(shape=(None, x[0], x[1]),
+										input_var=input_var)
+	
+	network = lasagne.layers.Conv1DLayer(
+			network, num_filters=512, filter_size=3,
+			nonlinearity=lasagne.nonlinearities.rectify,
+			W=lasagne.init.GlorotUniform())
+
+	network = lasagne.layers.DenseLayer(
+			lasagne.layers.dropout(network, p=.5),
+			num_units=16,
+			nonlinearity=lasagne.nonlinearities.rectify)
+
+	network = lasagne.layers.DenseLayer(
+			lasagne.layers.dropout(network, p=.5),
+			num_units=y,
+			nonlinearity=lasagne.nonlinearities.softmax)
+
+	return network
