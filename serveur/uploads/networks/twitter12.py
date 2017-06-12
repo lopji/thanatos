abfd4256e1d3131e26b16f078e7805212ad79aba
rocha <rochatj.contact@gmail.com>
2017-06-12 13:14:27 +0200

    Add twitter 12

--- serveur/uploads/networks/twitter12.py
+++ serveur/uploads/networks/twitter12.py
@@ -3,32 +3,27 @@ def build_cnn(x, y, input_var=None):
 	
 	network = lasagne.layers.InputLayer(shape=(None, x[0], x[1], x[2]),
 										input_var=input_var)
-
+										
 	network = lasagne.layers.Conv2DLayer(
-			network, num_filters=16, filter_size=(6, 6),
+			network, num_filters=8, filter_size=(5, 5),
 			nonlinearity=lasagne.nonlinearities.rectify,
 			W=lasagne.init.GlorotUniform())
 										
 	network = lasagne.layers.Conv2DLayer(
-			network, num_filters=32, filter_size=(5, 5),
-			nonlinearity=lasagne.nonlinearities.rectify,
-			W=lasagne.init.GlorotUniform())
-										
-	network = lasagne.layers.Conv2DLayer(
-			network, num_filters=64, filter_size=(4, 4),
+			network, num_filters=16, filter_size=(4, 4),
 			nonlinearity=lasagne.nonlinearities.rectify,
 			W=lasagne.init.GlorotUniform())
 				
 	network = lasagne.layers.Conv2DLayer(
-			network, num_filters=128, filter_size=(3, 3),
+			network, num_filters=32, filter_size=(3, 3),
 			nonlinearity=lasagne.nonlinearities.rectify)
 	
 	network = lasagne.layers.Conv2DLayer(
-			network, num_filters=256, filter_size=(2, 2),
+			network, num_filters=64, filter_size=(2, 2),
 			nonlinearity=lasagne.nonlinearities.rectify)
 	
 	network = lasagne.layers.Conv2DLayer(
-			network, num_filters=512, filter_size=(1, 1),
+			network, num_filters=128, filter_size=(1, 1),
 			nonlinearity=lasagne.nonlinearities.rectify)	
 	
 	network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
